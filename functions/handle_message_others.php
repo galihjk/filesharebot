303129d0401caa529bf1666b7cf7106e56c6ec9e
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-05-17 07:21:00 +0700

    print botdata for admin

--- functions/handle_message_others.php
+++ functions/handle_message_others.php
@@ -2,10 +2,19 @@
 function handle_message_others($botdata){
     $from_id = $botdata["from"]["id"];
     $chat_id = $botdata["chat"]["id"];
-    f("bot_kirim_perintah")("sendMessage",[
-        "chat_id"=>$chat_id,
-        "text"=>"/start",
-        "parse_mode"=>"MarkDown",
-    ]);
+    if(in_array($from_id,f("get_config")("bot_admins"))){
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>print_r($botdata,true),
+        ]);
+    }
+    else{
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>"/start",
+            "parse_mode"=>"MarkDown",
+        ]);
+    }
+    
     return true;
 }
