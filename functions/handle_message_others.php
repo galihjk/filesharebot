2f485ae679a4e525f4cf872c85afb5a32dfe5929
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-05-17 11:15:42 +0700

    ok1

--- functions/handle_message_others.php
+++ functions/handle_message_others.php
@@ -3,15 +3,22 @@ function handle_message_others($botdata){
     $from_id = $botdata["from"]["id"];
     $chat_id = $botdata["chat"]["id"];
     $message_id = $botdata["message_id"];
-    if(in_array($from_id,f("get_config")("bot_admins"))){
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>$chat_id,
-            "text"=>print_r($botdata,true),
-        ]);
-    }
+    $link = "t.me/".f("get_config")("botuname")."?start=$chat_id"."_".$message_id;
+    // if(in_array($from_id,f("get_config")("bot_admins"))){
+    //     f("bot_kirim_perintah")("sendMessage",[
+    //         "chat_id"=>$chat_id,
+    //         "text"=>print_r($botdata,true),
+    //     ]);
+    // }
+    f("bot_kirim_perintah")("sendMessage",[
+        "chat_id"=>$f("get_config")("channel_id"),
+        "text"=>$link . " " . print_r($botdata,true),
+        "reply_to_message_id"=>$message_id,
+    ]);
     f("bot_kirim_perintah")("sendMessage",[
         "chat_id"=>$chat_id,
-        "text"=>"t.me/".f("get_config")("botuname")."?start=$chat_id"."_".$message_id,
+        "text"=>$link,
+        "reply_to_message_id"=>$message_id,
     ]);
     
     return true;
