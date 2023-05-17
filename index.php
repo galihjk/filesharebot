1162cd243773f009ee230e52a19443125aef8d40
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-05-17 07:13:49 +0700

    INDEX

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "<h1>CoinBot</h1>";
-echo "<p><a href='setwebhook.php'>setwebhook</a></p>";
+echo "<h1>FileShareBot</h1>";
+if(file_exists('config.php')){
+    echo "<p><a href='setwebhook.php'>setwebhook</a></p>";
+}
+else{
+    echo "CONFIG FILE DOES NOT EXIST!";
+}
