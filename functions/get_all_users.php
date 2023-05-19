1a4663293d21ba473a45813e74c0b715793fad36
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-05-19 13:06:25 +0700

    broadcast

--- functions/get_all_users.php
+++ functions/get_all_users.php
@@ -1,9 +1,13 @@
 <?php
 function get_all_users(){
-    $scanned_directory = [];
+    $users = [];
     $folder = "data/users";
     if (is_dir($folder)){
-        $scanned_directory = array_diff(scandir($folder), array('..', '.'));
+        foreach(scandir($folder) as $file){
+            if(!in_array($file,['..', '.'])){
+                $users[] = substr($file, 0,-5);
+            }
+        }
     }
-    return $scanned_directory;
+    return $users;
 }
--- functions/handle_message_admin.php
+++ functions/handle_message_admin.php
@@ -16,8 +16,23 @@ function handle_message_admin($botdata){
             $all_user = f("get_all_users")();
             f("bot_kirim_perintah")("sendMessage",[
                 "chat_id"=>$botdata["chat"]["id"],
-                "text"=>print_r($all_user,true),
+                "text"=>"Broadcast dimulai. Harap tunggu sampai selesai.",
             ]);
+            $cnt = 0;
+            foreach($all_user as $item){
+                f("bot_kirim_perintah")("copyMessage",[
+                    "chat_id"=>$item,
+                    "from_chat_id"=>$botdata['reply_to_message']["chat"]["id"],
+                    "message_id"=>$botdata['reply_to_message']["message_id"],
+                ]);
+                $cnt++;
+                usleep(2000);
+            }
+            f("bot_kirim_perintah")("sendMessage",[
+                "chat_id"=>$botdata["chat"]["id"],
+                "text"=>"Broadcast selesai ($cnt).",
+            ]);
+            
         }
         return true;
     }
