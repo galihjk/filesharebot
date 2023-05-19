18ed367423d4f7e78fb2fe7d69c35fe349d2b23e
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-05-19 15:12:46 +0700

    ok 5

--- functions/handle_message_getfile.php
+++ functions/handle_message_getfile.php
@@ -3,11 +3,6 @@ function handle_message_getfile($botdata){
     if(!empty($botdata['text']) and f("str_is_diawali")($botdata['text'],"/start ")){
         $get_msg_id = str_replace("/start ","",$botdata['text']);
         $chat_id = $botdata["chat"]["id"];
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>$chat_id,
-            "text"=>f("get_config")("storage") . "~$get_msg_id",
-            "disable_web_page_preview"=>true,
-        ]);
 
         $result = f("bot_kirim_perintah")("copyMessage",[
             "chat_id"=>$chat_id,
